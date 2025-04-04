bf568c476852cda714d6334c6b4bcc0c065e5426
Bafko <rasta0760@gmail.com>
2025-04-04 21:27:56 +0300

    11

--- DownloaderApp/obj/Debug/net9.0-windows/DownloaderApp.AssemblyInfo.cs
+++ DownloaderApp/obj/Debug/net9.0-windows/DownloaderApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DownloaderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+081004b1ed130091446ce07408573dd7223eb180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa5c93211788b18235a3b672c8381a142adfb07")]
 [assembly: System.Reflection.AssemblyProductAttribute("DownloaderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DownloaderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
